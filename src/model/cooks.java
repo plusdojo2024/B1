349631fdcd2afed536a89cbade2e68e6bd1ce5c2
sekinumahibiki <sekinuma-hibiki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 16:13:50 +0900

    gomennne

--- src/model/cooks.java
+++ src/model/cooks.java
@@ -4,75 +4,112 @@ import java.io.Serializable;
 
 public class cooks implements Serializable {
 	private int cook_num ; /* 番号 */
-	private String cook_date  ; /* 日付 */
-	private String cook_img;  /* 画像 */
+	private int cook_date  ; /* 日付 */
+	private char cook_img ;  /* 画像 */
 	private String cook_name ; /* 料理名 */
 	private String cook_time;/* 調理時間 */
 	private String  cook_sta;  /* 開始時間 */
 	private String  cook_fin;   /* 終了時間 */
-	private String ;   /* 終了時間 */
-	private String cook_fin;   /* 終了時間 */
-	private String cook_fin;   /* 終了時間 */
-	private String cook_fin;   /* 終了時間 */
-	private String cook_fin;   /* 終了時間 */
+	private String  food_con ;   /* 	食材条件 */
+	private String food_used;   /* 使用した食材 */
+	private String seas_con ;   /* 調味料条件 */
+	private String seas_used;   /* 使用した調味料*/
+	private String cook_satis;   /* 調理の満足度 */
+	private String  aji_satis ;   /* 味の満足度 */
+	private String cook_fav;   /* お気に入り */
+	private String cook_com; /* コメント */
 
 
-	//引数がないコンストラクタ
-	public Bc() {
 
+
+	public int getCook_num() {
+		return cook_num;
 	}
-    //引数があるコンストラクタ
-	public Bc(int number, String company, String name, String address, String phone, String email, String remarks) {
-		super();
-		this.number = number;
-		this.company = company;
-		this.name = name;
-		this.address = address;
-		this.phone = phone;
-		this.email = email;
-		this.remarks = remarks;
+	public void setCook_num(int cook_num) {
+		this.cook_num = cook_num;
 	}
-	public int getNumber() {
-		return number;
+	public int getCook_date() {
+		return cook_date;
 	}
-	public void setNumber(int number) {
-		this.number = number;
+	public void setCook_date(int cook_date) {
+		this.cook_date = cook_date;
 	}
-	public String getCompany() {
-		return company;
+	public char getCook_img() {
+		return cook_img;
 	}
-	public void setCompany(String company) {
-		this.company = company;
+	public void setCook_img(char cook_img) {
+		this.cook_img = cook_img;
 	}
-	public String getName() {
-		return name;
+	public String getCook_name() {
+		return cook_name;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setCook_name(String cook_name) {
+		this.cook_name = cook_name;
 	}
-	public String getAddress() {
-		return address;
+	public String getCook_time() {
+		return cook_time;
 	}
-	public void setAddress(String address) {
-		this.address = address;
+	public void setCook_time(String cook_time) {
+		this.cook_time = cook_time;
 	}
-	public String getPhone() {
-		return phone;
+	public String getCook_sta() {
+		return cook_sta;
 	}
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public void setCook_sta(String cook_sta) {
+		this.cook_sta = cook_sta;
 	}
-	public String getEmail() {
-		return email;
+	public String getCook_fin() {
+		return cook_fin;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+	public void setCook_fin(String cook_fin) {
+		this.cook_fin = cook_fin;
 	}
-	public String getRemarks() {
-		return remarks;
+	public String getFood_con() {
+		return food_con;
 	}
-	public void setRemarks(String remarks) {
-		this.remarks = remarks;
+	public void setFood_con(String food_con) {
+		this.food_con = food_con;
+	}
+	public String getFood_used() {
+		return food_used;
+	}
+	public void setFood_used(String food_used) {
+		this.food_used = food_used;
+	}
+	public String getSeas_con() {
+		return seas_con;
+	}
+	public void setSeas_con(String seas_con) {
+		this.seas_con = seas_con;
+	}
+	public String getSeas_used() {
+		return seas_used;
+	}
+	public void setSeas_used(String seas_used) {
+		this.seas_used = seas_used;
+	}
+	public String getCook_satis() {
+		return cook_satis;
+	}
+	public void setCook_satis(String cook_satis) {
+		this.cook_satis = cook_satis;
+	}
+	public String getAji_satis() {
+		return aji_satis;
+	}
+	public void setAji_satis(String aji_satis) {
+		this.aji_satis = aji_satis;
+	}
+	public String getCook_fav() {
+		return cook_fav;
+	}
+	public void setCook_fav(String cook_fav) {
+		this.cook_fav = cook_fav;
+	}
+	public String getCook_com() {
+		return cook_com;
+	}
+	public void setCook_com(String cook_com) {
+		this.cook_com = cook_com;
 	}
 
-}
