d0e37964cb151199a389ddf7b5085a1e54d7eb2b
plusdojo-sawada <sawada-nonoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 17:57:05 +0900

    6/18 澤田更新分

--- src/servlet/foodSeasList.java
+++ src/servlet/foodSeasList.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/foodSeasList")
 public class foodSeasList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -21,6 +22,7 @@ public class foodSeasList extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
