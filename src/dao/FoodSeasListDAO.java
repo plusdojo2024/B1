c90fe7b5204cd7baa60ffb5814b4945f25e063cf
plusdojo-sawada <sawada-nonoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 15:26:42 +0900

    助けて

--- src/dao/FoodSeasListDAO.java
+++ src/dao/FoodSeasListDAO.java
@@ -1,56 +1,23 @@
 package dao;
 
-/**
- * Servlet implementation class FoodSeasListDAO
- */
-@WebServlet("/FoodSeasListDAO")
 
-public class FoodSeasListDAO extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+//ここから編集
+	public static String[] fsl(String genre) {
+//DBアクセス権
+	Connection conn = null;
+	String[] fslList = new String[4];
 
-//    /**
-//     * @see HttpServlet#HttpServlet()
-//     */
-//    public FoodSeasListDAO() {
-//        super();
-//        // TODO Auto-generated constructor stub
-// //   }
+	try {
+//データベース読み込み
+	Class.forName("org.h2.Driver");
+//データベースに接続
+	conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/", "B1", "");
 
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//	}
-//
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		doGet(request, response);
-////	}
-//
-////ここから編集
-//	public static String[] fsl(String genre) throws SQLException {
-//		Connection conn = null;
-//		String[] fslList = new String[4];
-
-		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/", "B1", "");
-
+//SQL準備
 	String sql = "SELECT * FROM food_seas WHERE meat;";
 	PreparedStatement pStmt = conn.prepareStatement(sql);
 
-
-
-	//SQL文を完成させる
-	if (fls.getName() != null) {	pStmt.setString(1, "%" + fls.getName() + "%");
-	//}
-	else {
-		pStmt.setString(1, "%");
-	}
-	if (fls.getAddress() != null) {
-		pStmt.setString(2, "%" + fls.getAddress() + "%");
-	}
-	else {
+//SQL文を実行し、結果表を記録する
+//結果表をコレクションにコピーする
+//データベースの切断
+//結果を返す
