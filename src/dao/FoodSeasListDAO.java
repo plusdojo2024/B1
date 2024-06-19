882ecac11b65eb756b9fc0da96eb628462fe1b6f
plusdojo-sawada <sawada-nonoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 14:53:16 +0900

    ごめん

--- src/dao/FoodSeasListDAO.java
+++ src/dao/FoodSeasListDAO.java
@@ -3,11 +3,11 @@ package dao;
 /**
  * Servlet implementation class FoodSeasListDAO
  */
-//@WebServlet("/FoodSeasListDAO")
+@WebServlet("/FoodSeasListDAO")
 
-//public class FoodSeasListDAO extends HttpServlet {
-//	private static final long serialVersionUID = 1L;
-//
+public class FoodSeasListDAO extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
 //    /**
 //     * @see HttpServlet#HttpServlet()
 //     */
@@ -36,22 +36,21 @@ package dao;
 //	public static String[] fsl(String genre) throws SQLException {
 //		Connection conn = null;
 //		String[] fslList = new String[4];
-//
-//		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/", "B1", "");
-//
-//	String sql = "SELECT * FROM food_seas WHERE meat;";
-//	PreparedStatement pStmt = conn.prepareStatement(sql);
-//
-//
-//
-//	//SQL文を完成させる
-//	if (fls.getName() != null) {
-//		pStmt.setString(1, "%" + fls.getName() + "%");
+
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/", "B1", "");
+
+	String sql = "SELECT * FROM food_seas WHERE meat;";
+	PreparedStatement pStmt = conn.prepareStatement(sql);
+
+
+
+	//SQL文を完成させる
+	if (fls.getName() != null) {	pStmt.setString(1, "%" + fls.getName() + "%");
 	//}
-//	else {
-//		pStmt.setString(1, "%");
-//	}
-//	if (fls.getAddress() != null) {
-//		pStmt.setString(2, "%" + fls.getAddress() + "%");
-//	}
-//	else {
+	else {
+		pStmt.setString(1, "%");
+	}
+	if (fls.getAddress() != null) {
+		pStmt.setString(2, "%" + fls.getAddress() + "%");
+	}
+	else {
