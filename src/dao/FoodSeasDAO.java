45dd1d7062b2cd82424de485d63137c4ee8aba48
plusdojo2024-ikiHaruto <iki-haruto-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 09:06:17 +0900

    定期

--- src/dao/FoodSeasDAO.java
+++ src/dao/FoodSeasDAO.java
@@ -9,6 +9,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FoodSeasDAO {
+    static {
+        try {
+            // データベースドライバの読み込み
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("H2ドライバの読み込みに失敗しました", e);
+        }
+    }
+
     public List<String> getRandomFoodSeasNamesByGenreAndStock() {
         List<String> foodSeasNames = new ArrayList<>();
 
@@ -16,11 +26,8 @@ public class FoodSeasDAO {
         String sqlVegetable = "SELECT food_seas_name FROM food_seas WHERE food_seas_genre = 'vege' AND food_seas_stock = TRUE ORDER BY RANDOM() LIMIT 2";
 
         try (
-        	//データベース読み込み
-        	Class.forName("org.h2.Driver");
-
-    		//データベースに接続
-        	Connection conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/B1/DB/B1","B1","");
+            // データベースに接続
+            Connection conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/B1/B1","B1","");
             PreparedStatement pstmtMeat = conn.prepareStatement(sqlMeat);
             PreparedStatement pstmtVegetable = conn.prepareStatement(sqlVegetable)) {
 
@@ -37,13 +44,6 @@ public class FoodSeasDAO {
                     foodSeasNames.add(rsVegetable.getString("food_seas_name"));
                 }
             }
-            // 地域名でグループ化された調味料名をすべて取得し、利用可能なものだけ明るくなどする。
-            //
-
-
-            // お助けアイテムを取得
-
-
 
         } catch (SQLException e) {
             e.printStackTrace();
