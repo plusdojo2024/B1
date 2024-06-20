6e123231bfcd835f6a629444c22e381e7e2b8974
plusdojo2024-ikiHaruto <iki-haruto-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 15:43:27 +0900

    0進捗

--- WebContent/WEB-INF/jsp/temp_regist.jsp
+++ WebContent/WEB-INF/jsp/temp_regist.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>仮登録</title>
 </head>
 <body>
 仮登録画面
--- src/servlet/SaveTemporaryRegistrationServlet.java
+++ src/servlet/SaveTemporaryRegistrationServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class SaveTemporaryRegistration
  */
-@WebServlet("/SaveTemporaryRegistration")
+@WebServlet("/SaveTemporaryRegistrationServlet")
 public class SaveTemporaryRegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
