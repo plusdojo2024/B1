a8d1e23503bac6106dde73fd1543d1e576b65701
plusdojo-sawada <sawada-nonoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-21 17:47:16 +0900

    06/21　退勤前

--- src/servlet/FoodSeasListServlet.java
+++ src/servlet/FoodSeasListServlet.java
@@ -10,11 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.BcDAO;
 import dao.FoodSeasListDAO;
-import model.Bc;
 import model.FoodSeasListmodel;
-import model.Result;
 
 /**
  * Servlet implementation class foodSeasList
@@ -26,7 +23,8 @@ public class FoodSeasListServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		// 検索処理を行う
 		FoodSeasListDAO fDao = new FoodSeasListDAO();
@@ -35,52 +33,58 @@ public class FoodSeasListServlet extends HttpServlet {
 		List<FoodSeasListmodel> fslList = fDao.select(food_seas_genre);
 
 		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("vegeList",fslList);
+		request.setAttribute("vegeList", fslList);
 
-		 food_seas_genre = "meat";
-		 fslList = fDao.select(food_seas_genre);
-
-		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("meatList",fslList);
-
-		 food_seas_genre = "help";
+		food_seas_genre = "meat";
 		fslList = fDao.select(food_seas_genre);
 
 		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("helpList",fslList);
+		request.setAttribute("meatList", fslList);
 
-		food_seas_genre = "myse";
-		 fslList = fDao.select(food_seas_genre);
+		food_seas_genre = "help";
+		fslList = fDao.select(food_seas_genre);
 
 		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("myseList",fslList);
+		request.setAttribute("helpList", fslList);
 
-//food_seas_genreの中身がmeat、vege、help,seasで、fs
+		food_seas_genre = "myse";
+		fslList = fDao.select(food_seas_genre);
 
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("myseList", fslList);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/food_seas_list.jsp");
+		dispatcher.forward(request, response);
 
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		////各食材ごとに削除ボタンを作成する
+		//		FoodSeasListDAO fDao = new FoodSeasListDAO();
+		//		if(request.getParameter("submit").equals("削除")) {
+		//			request.setAttribute("result",
+		//			new Result("削除成功！", "レコードを削除しました。", "/B1//FoodSeasListServlet"));
+		//		}
+		//		else {						// 削除失敗
+		//			request.setAttribute("result",
+		//			new Result("削除失敗！", "レコードを削除できませんでした。", "/B1//FoodSeasListServlet"));
+		//			}
 
-//チェックボックスがチェックされたら削除する
-	// 更新または削除を行う
-	FoodSeasListDAO fDao = new FoodSeasListDAO();
-	if (request.getParameter("meatdel").equals("在庫の削除")){
-		if(meatdel.checked) {
+		////チェックボックスがチェックされたら削除する
+		//	// 更新または削除を行う
+		//	FoodSeasListDAO fDao = new FoodSeasListDAO();
+		//	if (request.getParameter("meatdel").equals("在庫の削除")){
+		//		if(meatdel.checked) {
+		//
+		//		}
+		//	}
 
-		}
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/food_seas_list.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
-
-
-	// 結果ページにフォワードする
-	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/food_seas_list.jsp");
-	dispatcher.forward(request, response);
 }
-
-	}
-
-
-
-		}
--- src/servlet/FoodSeasRegistServlet.java
+++ src/servlet/FoodSeasRegistServlet.java
@@ -1,41 +1,52 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BcDAO;
+import model.Bc;
+import model.Result;
+
 /**
- * Servlet implementation class foodSeasRegist
+ * Servlet implementation class foodSeasList
  */
-@WebServlet("/foodSeasRegist")
+@WebServlet("/FoodSeasRegistServlet")
 public class FoodSeasRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FoodSeasRegistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/food_seas_regist.jsp");
+		dispatcher.forward(request, response);
+
+		// 登録処理を行う
+				BcDAO bDao = new BcDAO();
+				if (bDao.insert(new Bc(0, name, address))) {	// 登録成功
+					request.setAttribute("result",
+					new Result("登録成功！", "レコードを登録しました。", "/simpleBC/MenuServlet"));
+				}
+				else {												// 登録失敗
+					request.setAttribute("result",
+					new Result("登録失敗！", "レコードを登録できませんでした。", "/simpleBC/MenuServlet"));
+				}
+
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+				dispatcher.forward(request, response);
+			}
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		}
 
-}
