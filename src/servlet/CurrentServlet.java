bb2dcea0ed7d2188ee945db4def8b074a387d4f0
plusdojo2024-tajima <tajima-kihaya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-25 08:45:36 +0900

    スコア表示

--- src/servlet/CurrentServlet.java
+++ src/servlet/CurrentServlet.java
@@ -1,42 +1,34 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Current
- */
+import dao.ScoreDAO;
+import model.NowTourScore;
+
 @WebServlet("/CurrentServlet")
 public class CurrentServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CurrentServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	// 検索処理を行う
+    	ScoreDAO sDao = new ScoreDAO();
+    	// ここを改造しました
+    	int scores_num=1;
+    	List<NowTourScore> scoreList = sDao.select(scores_num);
+
+    	// 検索結果をリクエストスコープに格納する
+    	request.setAttribute("scoreList", scoreList);
+
+    	// メニューページにフォワードする
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/today_scores.jsp");
+    	dispatcher.forward(request, response);
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
